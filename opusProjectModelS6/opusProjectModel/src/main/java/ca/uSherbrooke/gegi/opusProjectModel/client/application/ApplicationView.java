6170a0bebe15ca89a87c2f6acd19caff2ee889a5
mathieu <mathieu@Mathieu-PC>
2015-05-25 10:54:47 -0400

    Version 1 du menubar

--- opusProjectModelS6/opusProjectModel/src/main/java/ca/uSherbrooke/gegi/opusProjectModel/client/application/ApplicationView.java
+++ opusProjectModelS6/opusProjectModel/src/main/java/ca/uSherbrooke/gegi/opusProjectModel/client/application/ApplicationView.java
@@ -5,7 +5,8 @@ import javax.inject.Inject;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 
@@ -13,8 +14,8 @@ class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     interface Binder extends UiBinder<Widget, ApplicationView> {
     }
 
-    @UiField
-    SimplePanel main;
+    @UiField VerticalPanel mainVertical;
+    @UiField MenuBar menuBar;
 
     @Inject
     ApplicationView(Binder uiBinder) {
@@ -24,7 +25,7 @@ class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     @Override
     public void setInSlot(Object slot, IsWidget content) {
         if (slot == ApplicationPresenter.SLOT_SetMainContent) {
-            main.setWidget(content);
+//        	mainVertical.setWidget(content);
         } else {
             super.setInSlot(slot, content);
         }
